5dd05f80072e4501aaae5b032956a14fddeaadf5
shanmukhaswamymn <92967496+shanmukhaswamymn@users.noreply.github.com>
2021-12-22 09:58:56 +0530

    Type casting

---
+++ Typecasting/bin/com/xworkz/typecasting/Typecasting/src/com/xworkz/typecasting/Typecasting.java
@@ -0,0 +1,109 @@
+package com.xworkz.typecasting;
+
+public class Typecasting {
+	
+	
+	byte number=85;
+	short short_num=number;
+	double double_num=number;
+	int int_num=number;
+	long long_num=number;
+	float float_num=number;
+    char char_num=(char) number;
+	
+	public String printAll() {
+		return "Typecasting [number=" + number + ", short_num=" + short_num + ", double_num=" + double_num
+				+ ", int_num=" + int_num + ", long_num=" + long_num + ", float_num=" + float_num + ", char_num="
+				+ char_num + "]";
+	}
+	short number1=89;
+	byte byte_num1=(byte)number1;
+	double double_num1=number1;
+	int int_num1=number1;
+	long long_num1=number1;
+	float float_num1=number1;
+    char char_num1=(char) number1;
+
+
+	public String printAll1() {
+		return "Typecasting [number1=" + number1 + ", byte_num1=" + byte_num1 + ", double_num1=" + double_num1
+				+ ", int_num1=" + int_num1 + ", long_num1=" + long_num1 + ", float_num1=" + float_num1 + ", char_num1="
+				+ char_num1 + "]";
+	}
+	
+	double number2=8569.25d;
+	short short_num2=(short) number2;
+	byte byte_num2=(byte) number2;
+	
+	int int_num2=(int) number2;
+	long long_num2=(long) number2;
+	float float_num2=(float) number2;
+    char char_num2= (char) number2;
+
+	
+	public String printAll2() {
+		return "Typecasting [number2=" + number2 + ", short_num2=" + short_num2 + ", byte_num2=" + byte_num2
+				+ ", int_num2=" + int_num2 + ", long_num2=" + long_num2 + ", float_num2=" + float_num2 + ", char_num2="
+				+ char_num2 + "]";
+	}
+	int number3=8950789;
+	double num_num3=number3;
+	short short_num3=(short) number3;
+	byte byte_num3=(byte) number3;
+	long long_num3=(long) number3;
+	float float_num3=(float) number3;
+    char char_num3= (char) number3;
+
+	
+	public String printAll3() {
+		return "Typecasting [number3=" + number3 + ", num_num3=" + num_num3 + ", short_num3=" + short_num3
+				+ ", byte_num3=" + byte_num3 + ", long_num3=" + long_num3 + ", float_num3=" + float_num3
+				+ ", char_num3=" + char_num3 + "]";
+	}
+	long number4=759489;
+	double double_num4=number4;
+	short short_num4=(short) number4;
+	byte byte_num4=(byte) number4;
+	int int_num4=(int) number4;
+	float float_num4=(float) number4;
+    char char_num4= (char) number4;
+
+
+	public String printAll4() {
+		return "Typecasting [number4=" + number4 + ", double_num4=" + double_num4 + ", short_num4=" + short_num4
+				+ ", byte_num4=" + byte_num4 + ", int_num4=" + int_num4 + ", float_num4=" + float_num4 + ", char_num4="
+				+ char_num4 + "]";
+	}
+	float number5=(float) 4488.45;
+	long long_num5=(long) number5;
+	double douuble_num5=number5;
+	short short_num5=(short) number5;
+	byte byte_num5=(byte) number5;
+	int int_num5=(int) number5;
+	float float_num5=(float) number5;
+    char char_num5= (char) number5;
+
+	
+	public String printAll5() {
+		return "Typecasting [number5=" + number5 + ", long_num5=" + long_num5 + ", douuble_num5=" + douuble_num5
+				+ ", short_num5=" + short_num5 + ", byte_num5=" + byte_num5 + ", int_num5=" + int_num5 + ", float_num5="
+				+ float_num5 + ", char_num5=" + char_num5 + "]";
+	}
+	char number6=45;
+	float float_num6=(float) 4488.45;
+	long long_num6=(long) number6;
+	double douuble_num6=number6;
+	short short_num6=(short) number6;
+	byte byte_num6=(byte) number6;
+	int int_num6=(int) number6;
+
+
+	public String printAll6() {
+		return "Typecasting [number6=" + number6 + ", float_num6=" + float_num6 + ", long_num6=" + long_num6
+				+ ", douuble_num6=" + douuble_num6 + ", short_num6=" + short_num6 + ", byte_num6=" + byte_num6
+				+ ", int_num6=" + int_num6 + "]";
+	}
+	
+    
+
+}
