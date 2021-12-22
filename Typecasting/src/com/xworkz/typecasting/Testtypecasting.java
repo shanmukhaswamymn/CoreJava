130bb6eac840e106ac36cde5b06449270711762f
shanmukhaswamymn <92967496+shanmukhaswamymn@users.noreply.github.com>
2021-12-22 09:02:55 +0530

    Typecasting

---
+++ Typecasting/src/com/xworkz/typecasting/Testtypecasting.java
@@ -0,0 +1,18 @@
+package com.xworkz.typecasting;
+
+public class Testtypecasting {
+	
+	public static void main(String[] args) {
+		
+		Typecasting casting=new Typecasting();
+		
+		System.out.println(casting.printAll());
+		System.out.println(casting.printAll1());
+		System.out.println(casting.printAll2());
+		System.out.println(casting.printAll3());
+		
+		
+	}
+	
+
+}
